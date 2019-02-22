4107883a4db071478b2130d1a26a316537e4ec87
Brady Mann <Football3320>
2019-02-22 14:13:56 -0500

    created a method to print out Hello with any word in the printGreetings

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,10 +5,15 @@ public class Main {
     public static void main(String[] args) {
 
     printGreeting();
+    printGreetings("Lamp");
     }
 
     public static void printGreeting(){
         System.out.println("Hello World");
+    }
+
+    public static void printGreetings(String noun){
+        System.out.println("Hello "+noun);
     }
 
 
