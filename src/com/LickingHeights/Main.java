913195a890d86d83a559bcfd5de07bb4f6cdec18
Brady Mann <Football3320>
2019-02-22 14:06:23 -0500

    created Method Project

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,15 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+	
+
+    }
+
+    public static void printGreeting(){
+        System.out.println("Hello World");
+    }
+
+
+}
