b907aa1a8f71a46dfb3773a43abc676ae0515128
Brady Mann <Football3320>
2019-02-25 13:54:13 -0500

    made the divisional method

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -4,26 +4,33 @@ public class Main {
 
     public static void main(String[] args) {
 
-    printGreeting();
-    printGreetings("Lamp");
-    int a=17;
-    int b=24;
-    int c= multiply(a,b);
-    System.out.println(c);
+        printGreeting();
+        printGreetings("Lamp");
+        int a = 17;
+        int b = 24;
+        int c = multiply(a, b);
+        System.out.println(c);
+        double d = 42;
+        double e = 156;
+        double f = divide(e,d);
+        System.out.println(f);
     }
 
-    public static void printGreeting(){
+    public static void printGreeting() {
         System.out.println("Hello World");
     }
 
-    public static void printGreetings(String noun){
-        System.out.println("Hello "+noun);
+    public static void printGreetings(String noun) {
+        System.out.println("Hello " + noun);
     }
 
-    public static int multiply(int firstNumber, int secondNumber){
+    public static int multiply(int firstNumber, int secondNumber) {
 
-        return firstNumber*secondNumber;
+        return firstNumber * secondNumber;
     }
 
+    public static double divide(double firstInt, double secondInt) {
 
+        return firstInt / secondInt;
+    }
 }
